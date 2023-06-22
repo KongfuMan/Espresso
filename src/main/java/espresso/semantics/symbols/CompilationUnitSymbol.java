c5fc93b1cb3b989560832f302b6fa7869cc520e7
chenliangsjtu <chenliangsjtu@gmail.com>
2023-06-21 23:36:56 -0500

    Added java doc for CompilationUnitSymbol

--- src/main/java/espresso/semantics/symbols/CompilationUnitSymbol.java
+++ src/main/java/espresso/semantics/symbols/CompilationUnitSymbol.java
@@ -3,7 +3,11 @@ package espresso.semantics.symbols;
 import org.antlr.v4.runtime.ParserRuleContext;
 
 public class CompilationUnitSymbol extends Scope{
-
+    /**
+     * Top level scope representing a namespace.
+     * @param idName the identifier name of namespace, e.g. "system.utils"
+     * @param node the syntax node that represent namespace.
+     * */
     public CompilationUnitSymbol(String idName, ParserRuleContext node){
         super(idName, node, null);
     }
